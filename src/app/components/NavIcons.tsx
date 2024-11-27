c5c65a35678bf720c354603b119bbcf43e3a73a1
Agustín Mayer <54483431+agustin-mayer@users.noreply.github.com>
2024-11-27 17:18:00 -0300

    remove perfil icon

--- src/app/components/NavIcons.tsx
+++ src/app/components/NavIcons.tsx
@@ -44,23 +44,7 @@ const NavIcons = () => {
 
   return (
     <div className='flex items-center justify-end gap-8 xl:gap-6 relative'>
-      <Image
-        src='/profile.png'
-        alt='Profile'
-        width={22}
-        height={22}
-        className='cursor-pointer'
-        onClick={handleProfileClick}
-      />
-      {isAuthenticated && isProfileOpen && (
-        <div className='absolute p-4 rounded-md top-12 w-32 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20'>
-          <Link href='/profile'>Perfil</Link>
-          <div className='mt-2 cursor-pointer' onClick={handleLogout}>
-            Cerrar Sesión
-          </div>
-        </div>
-      )}
-      <div className='relative cursor-pointer'>
+      <div className='relative cursor-pointer mr-2'>
         <Image
           src='/cart.png'
           alt='Cart'
