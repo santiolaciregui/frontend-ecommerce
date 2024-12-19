60cb0d1e37a249499ede7e838f9f24e18756a02b
Agustín Mayer <mayeragustin99@gmail.com>
2024-12-19 01:47:38 -0300

    Update order-tracking template (more intuitive)

--- src/app/order-tracking/page.tsx
+++ src/app/order-tracking/page.tsx
@@ -6,7 +6,7 @@ import orderService from '../pages/api/order';
 import { FaSearch } from 'react-icons/fa'; // Font Awesome
 
 const SeguimientoPedido: React.FC = () => {
-  const [numeroPedido, setNumeroPedido] = useState('');
+  const [trackingId, setTrackingId] = useState('');
   const [cargando, setCargando] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -19,7 +19,7 @@ const SeguimientoPedido: React.FC = () => {
 
     try {
       // Busca las órdenes por número de orden
-      const orden = await orderService.fetchOrderById(numeroPedido);  // Pasa solo el número de orden
+      const orden = await orderService.fetchOrderByTrackingId(trackingId);  // Pasa solo el número de orden
 
       if (orden) {
         const idOrden = orden.id;
@@ -38,34 +38,42 @@ const SeguimientoPedido: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-green-50 py-12">
-      <div className="max-w-lg mx-auto bg-white p-8 shadow-xl rounded-lg">
-        <h2 className="text-2xl font-semibold text-center text-green-500 mb-6">Consulta el estado de tu pedido</h2>
-        <form onSubmit={handleSubmit} className="mb-8 space-y-4">
-          <div className="mb-6">
-            <label className="block text-gray-700 text-lg font-medium">Número de pedido</label>
-            <input
-              type="text"
-              value={numeroPedido}
-              onChange={(e) => setNumeroPedido(e.target.value)}
-              className="w-full p-3 border-2 border-green-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-              placeholder="Ingresa tu número de pedido"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-green-500 text-white p-3 rounded-lg hover:bg-green-700 flex items-center justify-center space-x-2"
-          >
-            <FaSearch className="text-xl" />
-            <span>Buscar</span>
-          </button>
-        </form>
-
-        {error && <p className="text-red-500 text-center">{error}</p>}
-
-        {cargando && <p className="text-center text-gray-600">Cargando...</p>}
-      </div>
+    <div className="max-w-lg mx-auto bg-white p-8 shadow-xl rounded-lg">
+      <h2 className="text-2xl font-semibold text-center text-green-500 mb-6">Consulta el estado de tu pedido</h2>
+      <p className="text-gray-700 mb-4 text-center">
+        Para consultar el estado de tu pedido, ingresa el <strong>número de seguimiento</strong> que te enviamos por correo electrónico
+        al momento de realizar la compra. Este número es único para tu pedido.
+      </p>
+      <form onSubmit={handleSubmit} className="mb-8 space-y-4">
+        <div className="mb-6">
+          <label className="block text-gray-700 text-lg font-medium">Número de seguimiento</label>
+          <input
+            type="text"
+            value={trackingId}
+            onChange={(e) => setTrackingId(e.target.value)}
+            className="w-full p-3 border-2 border-green-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+            placeholder="Ingresa tu número de seguimiento"
+            required
+          />
+          <small className="text-gray-500 mt-2 block">
+            Si no lo encuentras, revisa el correo electrónico que te enviamos después de realizar tu compra.
+          </small>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-green-500 text-white p-3 rounded-lg hover:bg-green-700 flex items-center justify-center space-x-2"
+        >
+          <FaSearch className="text-xl" />
+          <span>Buscar</span>
+        </button>
+      </form>
+  
+      {error && <p className="text-red-500 text-center">{error}</p>}
+  
+      {cargando && <p className="text-center text-gray-600">Cargando...</p>}
     </div>
+  </div>
+  
   );
 };
 
