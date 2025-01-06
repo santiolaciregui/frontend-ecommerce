35927ee264d2c0806d72b31fcf3653f8b77ab76e
santiolaciregui <54482877+santiolaciregui@users.noreply.github.com>
2025-01-06 10:35:28 -0300

    categories main page

--- src/app/components/CategoriesMenu.tsx
+++ src/app/components/CategoriesMenu.tsx
@@ -1,25 +1,37 @@
-// components/Categories.tsx
-interface Category {
-    name: string;
-    image: string;
-  }
-  
-  interface CategoriesProps {
-    categories: Category[];
-  }
-  
-  const CategoriesMenu: React.FC<CategoriesProps> = ({ categories }) => {
-    return (
-      <div className="mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {categories.map((category, idx) => (
-          <div key={idx} className="bg-white shadow-md rounded-lg p-4 hover:shadow-xl transition-shadow duration-300">
-            <h3 className="text-lg font-semibold">{category.name}</h3>
-            <img src={category.image} alt={category.name} className="w-full h-48 object-cover mt-2 rounded-lg" />
+import React from 'react';
+
+interface CategoriesProps {
+  categories: any[];
+  onCategoryClick: (categoryId: number) => void;
+}
+
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+const CategoriesMenu: React.FC<CategoriesProps> = ({ categories, onCategoryClick }) => {
+  return (
+    <div className="mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {categories.map((category) => (
+        <div
+          key={category.id}
+          className="bg-white shadow-md rounded-lg p-4 hover:shadow-xl transition-shadow duration-300 cursor-pointer"
+          onClick={() => onCategoryClick(category.id)} // Trigger on click
+        >
+          <h3 className="text-lg font-semibold">{category.name}</h3>
+          <div className="w-full aspect-w-16 aspect-h-9 mt-2 rounded-lg overflow-hidden">
+            <img
+              src={
+                category.previewImage
+                  ? `${API_URL}${category.previewImage}`
+                  : '/logo-verde-manzana.svg'
+              }
+              alt={category.name}
+              className="w-full h-full object-cover"
+            />
           </div>
-        ))}
-      </div>
-    );
-  };
-  
-  export default CategoriesMenu;
-  
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CategoriesMenu;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,48 +2,26 @@
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import PromoCarousel from './components/PromoCarousel';
-import { fetchCategories } from './pages/api/category';
-import ProductImages from './components/ProductImages';
-import { Category } from './context/types';
+import { fetchCategoriesDashboard } from './pages/api/category';
+import CategoriesMenu from './components/CategoriesMenu';
 
-const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL; // Ajusta según tu configuración
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL; // Adjust as per your config
 
 const HomePage = () => {
   const router = useRouter();
-  const [parentCategories, setParentCategories] = useState<Category[]>([]);
+  const [parentCategories, setParentCategories] = useState<any[]>([]);
 
   useEffect(() => {
     const loadParentCategories = async () => {
-      try {
-        const categoriesData = await fetchCategories();
-        const enrichedCategories = await Promise.all(
-          categoriesData.map(async (category: Category) => {
-            const res = await fetch(`${API_URL}/products/category/${category.id}`);
-            const products = await res.json();
-            const images = products[0]?.Images || []; // Todas las imágenes del primer producto
-            return {
-              ...category,
-              images,
-            };
-          })
-        );
-  
-        // Filtrar categorías que no tengan imágenes
-        const filteredCategories = enrichedCategories.filter(
-          (category) => category.images.length > 0
-        );
-  
-        setParentCategories(filteredCategories as Category[]);
-      } catch (error) {
-        console.error('Error fetching categories or products:', error);
-      }
+      const categoriesData = await fetchCategoriesDashboard();
+      setParentCategories(categoriesData);
     };
   
     loadParentCategories();
   }, []);
   
   const handleCategoryClick = (categoryId: number) => {
-    router.push(`/products?page=1&categoryId=${categoryId}`);
+    router.push(`/products?page=&category=${categoryId}`);
   };
 
   const promoImages = ['/carousel1.png', '/carousel2.png', '/carousel3.png'];
@@ -52,26 +30,12 @@ const HomePage = () => {
     <div className="text-center">
       {/* Promo Carousel */}
       <PromoCarousel images={promoImages} />
+      <br/>
 
       {/* Categories Section */}
       <div>
         <h1 className="text-2xl font-bold my-2">Nuestros Productos</h1>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {parentCategories.map((category) => (
-            <div
-              key={category.id}
-              onClick={() => handleCategoryClick(category.id)}
-              className="group relative cursor-pointer rounded-md overflow-hidden"
-            >
-              {/* Usando el componente ProductImages */}
-              <ProductImages items={[category]} />
-
-              <div className="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white text-sm px-2 py-1">
-                {category.name}
-              </div>
-            </div>
-          ))}
-        </div>
+        <CategoriesMenu categories={parentCategories} onCategoryClick={handleCategoryClick} />
       </div>
     </div>
   );
